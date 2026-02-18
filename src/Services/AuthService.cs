2054ea745d5d63092dffa823dbedbce69245ba1f
danglnh07 <danglnh07.work@gmail.com>
2026-02-18 23:32:57 +0700

    Refactor AuthService that support both cookie and token authentication with cleaner methods

--- src/Services/AuthService.cs
+++ src/Services/AuthService.cs
@@ -1,5 +1,3 @@
-using FluentValidation;
-using KidFit.Dtos;
 using KidFit.Models;
 using KidFit.Shared.Exceptions;
 using Microsoft.AspNetCore.Identity;
@@ -8,106 +6,45 @@ namespace KidFit.Services
 {
     public class AuthService(UserManager<ApplicationUser> userManager,
                              SignInManager<ApplicationUser> signInManager,
-                             IValidator<LoginRequestDto> loginValidator,
                              IConfiguration configuration,
                              ILogger<AuthService> logger)
     {
         private readonly UserManager<ApplicationUser> _userManager = userManager;
         private readonly SignInManager<ApplicationUser> _signInManager = signInManager;
-        private readonly IValidator<LoginRequestDto> _loginValidator = loginValidator;
         private readonly IConfiguration _configuration = configuration;
         private readonly ILogger<AuthService> _logger = logger;
 
-        public async Task<LoginResponseDto> LoginAsync(LoginRequestDto req, bool generateToken = false)
+        private async Task<ApplicationUser> GetAndValidateLoginCredentials(string? email, string? username)
         {
-            // Validate login request DTO
-            var validationResult = _loginValidator.Validate(req);
-            if (!validationResult.IsValid)
-            {
-                var message = "Failed to login: model validation failed";
-                List<string> errors = [.. validationResult.Errors.Select(e => e.ErrorMessage)];
-                throw Shared.Exceptions.ValidationException.Create(message, errors);
-            }
-
             // Find user by username or email
-            ApplicationUser? user = null;
-            if (req.Username is not null)
+            ApplicationUser? user;
+            if (!string.IsNullOrEmpty(username))
             {
-                user = await _userManager.FindByNameAsync(req.Username);
+                user = await _userManager.FindByNameAsync(username);
             }
-            else if (req.Email is not null)
+            else if (!string.IsNullOrEmpty(email))
             {
-                user = await _userManager.FindByEmailAsync(req.Email);
+                user = await _userManager.FindByEmailAsync(email);
             }
             else
             {
-                // Though validator already check it, we still add a log just in case
                 _logger.LogWarning($"Login failed: Username or email is required");
+                throw new ArgumentException("Either email or username must be provided");
             }
 
             // Check if user exist
             if (user is null)
             {
-                _logger.LogWarning($"Login failed: User {(req.Username is not null ? req.Username : req.Email)} not found");
                 throw IdentityException.Create("Login failed: incorrect login credentials");
             }
 
             // Check if user is active
             if (!user.IsActive)
             {
-                _logger.LogWarning($"Login failed: User {(req.Username is not null ? req.Username : req.Email)} is disabled");
                 throw IdentityException.Create("Login failed: account is disabled");
             }
 
-            // Check if password match
-            var result = await _signInManager.PasswordSignInAsync(user, req.Password, false, false);
-            if (!result.Succeeded)
-            {
-                _logger.LogWarning($"Login failed: Invalid password for user {(req.Username is not null ? req.Username : req.Email)}");
-                throw IdentityException.Create("Login failed: incorrect login credentials");
-            }
-
-            if (generateToken) return new();
-
-            // Get user roles
-            var roles = await _userManager.GetRolesAsync(user);
-
-            // Generate JWT token
-            var jwtParam = new JwtParams()
-            {
-                Identifier = user.Id,
-                Name = user.FullName,
-                Email = user.Email!, // Email is required when create user 
-                GivenName = user.UserName!, // Username is required when create user
-                Role = roles.FirstOrDefault() ?? "",
-                Issuer = GetJwtIssuer(),
-                Audience = GetJwtAudience(),
-                ExpiresInMinutes = GetTokenExpirationMinutes(),
-            };
-            var token = SecurityService.GenerateJWTToken(jwtParam, GetJwtSecret());
-
-            // Return login response with token and some basic user information
-            return new LoginResponseDto
-            {
-                Token = token,
-                Username = user.UserName!,
-                FullName = user.FullName,
-                Role = roles.FirstOrDefault() ?? "",
-                ExpiresAt = DateTimeOffset.UtcNow.AddMinutes(GetTokenExpirationMinutes())
-            };
-        }
-
-        public async Task ResetPasswordAsync(string id, string token, string newPassword)
-        {
-            // Find user by ID 
-            var user = await _userManager.FindByIdAsync(id) ?? throw NotFoundException.Create(typeof(ApplicationUser).Name);
-
-            // Reset password with token
-            var result = await _userManager.ResetPasswordAsync(user, token, newPassword);
-            if (!result.Succeeded || result.Errors.Count() != 0)
-            {
-                throw IdentityException.Create("Reset password failed", result.Errors);
-            }
+            return user;
         }
 
         private string GetJwtSecret()
@@ -128,6 +65,81 @@ namespace KidFit.Services
         private int GetTokenExpirationMinutes()
         {
             return _configuration.GetValue("AppSettings:ExpirationMinutes", 24);
+        }
+
+        public async Task LoginWithCookieAsync(string? email, string? username, string password)
+        {
+            var user = await GetAndValidateLoginCredentials(email, username);
+
+            // Check if password match
+            var result = await _signInManager.PasswordSignInAsync(user, password, false, false);
+            if (!result.Succeeded)
+            {
+                throw IdentityException.Create("Login failed: incorrect login credentials");
+            }
+        }
+
+        public async Task<string> LoginWithTokenAsync(string? email, string? username, string password)
+        {
+            var user = await GetAndValidateLoginCredentials(email, username);
+
+            // Check if password match
+            var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+            if (!result.Succeeded)
+            {
+                throw IdentityException.Create("Login failed: incorrect login credentials");
+            }
+
+            // Get user roles
+            var roles = await _userManager.GetRolesAsync(user);
+            if (roles is null || roles.Count() == 0)
+            {
+                throw IdentityException.Create("Generate token failed: user has no roles");
+            }
+
+            // Create JWT param 
+            var param = new JwtParams()
+            {
+                Identifier = user.Id,
+                Name = user.UserName!,
+                Email = user.Email!,
+                GivenName = user.FullName,
+                Role = roles.FirstOrDefault()!,
+                Issuer = GetJwtIssuer(),
+                Audience = GetJwtAudience(),
+                ExpiresInMinutes = GetTokenExpirationMinutes()
+            };
+
+            return SecurityService.GenerateJwtToken(param, GetJwtSecret());
+        }
+
+        // This method will logout user with Cookie based login method. 
+        // For JWT, it should be the UI side to handle that, as we didn't 
+        // have blacklist mechanism yet
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+
+        public async Task ResetPasswordAsync(string id, string token, string newPassword)
+        {
+            // Find user by ID 
+            var user = await _userManager.FindByIdAsync(id);
+            if (user is null)
+            {
+                throw NotFoundException.Create(typeof(ApplicationUser).Name);
+            }
+            else if (!user.IsActive)
+            {
+                throw ForbiddenException.Create("Reset password failed: account is disabled");
+            }
+
+            // Reset password with token
+            var result = await _userManager.ResetPasswordAsync(user, token, newPassword);
+            if (!result.Succeeded || result.Errors.Count() != 0)
+            {
+                throw IdentityException.Create("Reset password failed", result.Errors);
+            }
         }
     }
 }
